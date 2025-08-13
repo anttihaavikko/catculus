d359eafd1d1e11ebca9687da49cb7d7a18ca6a3c
Antti Haavikko <antti.haavikko@gmail.com>
2025-08-13 16:36:48 +0300

    Add basic game loop

--- src/scene.ts
+++ src/scene.ts
@@ -1,16 +1,89 @@
 import { Container } from './engine/container';
+import { Game } from './engine/game';
 import { Mouse } from './engine/mouse';
+import { randomCell } from './engine/random';
+import { TextEntity } from './engine/text';
+import { ZERO } from './engine/vector';
+import { Tile } from './tile';
+
+export const GRID_SIZE = 7;
 
 export class Scene extends Container {
+    private tiles: Tile[];
+    private picks: Tile[] = [];
+    private target: number;
+    private level: number = 0;
+    private locked: boolean;
+    private targetLabel: TextEntity;
+    private sumLabel: TextEntity;
+    
+    constructor(game: Game) {
+        super(game);
+
+        this.tiles = Array.from(Array(GRID_SIZE * GRID_SIZE)).map((_, i) => new Tile(game, i));
+
+        this.targetLabel = new TextEntity(game, '', 50, 500, 220, -1, ZERO);
+        this.sumLabel = new TextEntity(game, '', 50, 550, 220, -1, ZERO);
+        
+        this.add(...this.tiles, this.targetLabel, this.sumLabel);
+
+        this.findTarget();
+    }
+    
     public update(tick: number, mouse: Mouse): void {
         super.update(tick, mouse);
+
+        if (mouse.pressing && !this.locked) {
+            const tile = this.tiles.find(t => t.hovered);
+            if (tile && !tile.hidden && (this.picks.length === 0 || this.picks.some(t => t.isClose(tile)))) {
+                tile.picked = !tile.picked;
+                this.toggle(tile);
+
+                const sum = this.picks.reduce((acc, t) => acc + t.value, 0);
+                if (sum >= this.target) {
+                    console.log(`DONE, DIFF: ${sum - this.target}`);
+                    console.log('--------------');
+                    setTimeout(() => {
+                        this.picks.forEach(t => {
+                            t.picked = false;
+                            t.value++;
+                        });
+                        this.picks = [];
+                        this.findTarget();
+                    }, 500);
+                }
+            }
+        }
+    }
+
+    private findTarget(): void {
+        this.level++;
+        this.target = this.generateTarget(this.level + 1);
+        this.targetLabel.content = this.target.toString();
+    }
+
+    private generateTarget(count: number): number {
+        const cells = [randomCell(this.tiles.filter(t => !t.hidden))];
+        for (let i = 0; i < count - 1; i++) {
+            cells.push(randomCell(this.tiles.filter(t => !t.hidden && !cells.includes(t) && cells.some(c => c.isClose(t)))));
+        }
+        // console.log(`FROM: ${cells.map(t => t.value).join(', ')}`);
+        return cells.reduce((acc, t) => acc + t.value, 0);
+    }
+
+    private toggle(tile: Tile): void {
+        if (this.picks.includes(tile)) {
+            this.picks = this.picks.filter(t => t !== tile);
+            return;
+        }
+        this.picks.push(tile);
     }
 
     public draw(ctx: CanvasRenderingContext2D): void {
         ctx.save();
         ctx.fillStyle = '#333';
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.restore();
         super.draw(ctx);
+        ctx.restore();
     }
 }
---
+++ src/tile.ts
@@ -0,0 +1,58 @@
+import { font } from './engine/constants';
+import { Entity } from './engine/entity';
+import { Game } from './engine/game';
+import { Mouse } from './engine/mouse';
+import { distance } from './engine/vector';
+import { GRID_SIZE } from './scene';
+
+export const TILE_SIZE = 40;
+export const TILE_GAP = 6;
+
+export class Tile extends Entity {
+    public value: number;
+    public hovered: boolean;
+    public picked: boolean;
+    public hidden: boolean;
+
+    public constructor(game: Game, i: number) {
+        const size = TILE_SIZE + TILE_GAP;
+        const x = i % GRID_SIZE;
+        const y = Math.floor(i / GRID_SIZE);
+        super(game, 50 + x * size, 45 + y * size, TILE_SIZE, TILE_SIZE);
+        this.value = x - 1 + (y - 2) * 3;
+        if (x < 2 || x > 4 || y < 2 || y > 4) {
+            this.value = 1;
+            this.hidden = true;
+        }
+    }
+
+    public isClose(other: Tile): boolean {
+        return distance(this.p, other.p) < TILE_SIZE * 1.5;
+    }
+
+    public update(tick: number, mouse: Mouse): void {
+        this.hovered = this.isInside(mouse, 1);
+        super.update(tick, mouse);
+    }
+
+    public draw(ctx: CanvasRenderingContext2D): void {
+        if (this.hidden) return;
+        ctx.save();
+        ctx.fillStyle = this.hovered ? 'yellow' : '#fff';
+        if (this.picked) ctx.fillStyle = 'orange';
+        const outline = this.hovered ? 'red' : '#000';
+        ctx.strokeStyle = outline;
+        ctx.beginPath();
+        ctx.lineWidth = 7;
+        ctx.translate(this.p.x, this.p.y);
+        ctx.rect(0, 0, this.s.x, this.s.y);
+        ctx.stroke();
+        ctx.fill();
+        ctx.fillStyle = outline;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.font = `20px ${font}`;
+        ctx.fillText(this.value.toString(), this.s.x / 2, this.s.y / 2);
+        ctx.restore();
+    }
+}
