133f93691375e525d56002266033256e0a221310
Antti Haavikko <antti.haavikko@gmail.com>
2025-08-19 15:20:03 +0300

    Hilite ignore word on reflex skill

--- src/skills.ts
+++ src/skills.ts
@@ -10,6 +10,6 @@ export const skills: Skill[] = [
     { name: 'catnip', description: 'Cats |always jump| to\nthe |highest value| tile' },
     { name: 'catnap', description: '|3x| score for |sleeping| cats,\n|no bonus| for |awake| cats' },
     { name: 'sprayer', description: '|Awake| cats |reveal| the |tile\n |value| when selected' },
-    { name: 'reflexes', description: 'Any |mistake| equal to\nyour |total life| is ignored' },
+    { name: 'reflexes', description: 'Any |mistake| equal to\nyour |total life| is |ignored' },
     { name: 'nine', description: 'Regain a |life| for each |used 9|\n in the |calculation', repeatable: true },
 ];
